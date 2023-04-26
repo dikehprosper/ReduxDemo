e6d36cbad6b1870ecca68034f28ae78fe96e2f9a
prosper <dikehprosper@gmail.com>
2023-04-26 11:46:34 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isFirstRender = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification)
+  const cart = useSelector(state => state.cart);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+
+  useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
+    const sendRequest = async () => {
+      // send state as sending request
+      dispatch(uiActions.showNotifiation({
+        open: true,
+        message: 'sending request',
+        type: 'warning'
+      }))
+      const res = await fetch("https://redux-http-3e82c-default-rtdb.firebaseio.com/cartitems.json", {
+        method: "PUT",
+        body: JSON.stringify(cart)
+      });
+      const data = await res.json();
+      // send state as request is successful
+      dispatch(uiActions.showNotifiation({
+        open: true,
+        message: 'Sent Request To Database Successfully',
+        type: 'success'
+      }))
+    };
+    sendRequest().catch(err => {
+      // send state as error
+      dispatch(uiActions.showNotifiation({
+        open: true,
+        message: 'Sending Request To Database Failed',
+        type: 'error'
+      }))
+    })
+  }, [cart, dispatch]);
+
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {notification && <Notification type={notification.type} message={notification.message} />}
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import "./Cart.css";
-import { cartActions } from "./../store/cartSlice";
+import { cartActions } from "./../store/cart-slice";
+
 const CartItem = ({ name, quantity, total, price, id }) => {
   const dispatch = useDispatch();
-  const removeHandler = () => {
+  const removeFromCart = () => {
     dispatch(cartActions.removeFromCart(id));
   };
-  const addHandler = () => {
+  const addToCart = () => {
     dispatch(
       cartActions.addToCart({
         id,
@@ -19,13 +20,13 @@ const CartItem = ({ name, quantity, total, price, id }) => {
   return (
     <div className="cartItem">
       <h2> {name}</h2>
-      <p>${price} /-</p>
-      <p>x{quantity}</p>
+      <p>${price} </p>
+    <p>x{quantity}</p> 
       <article>Total ${total}</article>
-      <button className="cart-actions" onClick={removeHandler}>
+      <button className="cart-actions" onClick={removeFromCart}>
         -
       </button>
-      <button className="cart-actions" onClick={addHandler}>
+      <button className="cart-actions" onClick={addToCart}>
         +
       </button>
     </div>
