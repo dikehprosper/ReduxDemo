97e1157defd6e626e70c002348846d1a8a55515d
prosper <dikehprosper@gmail.com>
2023-05-04 09:12:09 +0100

    updated

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -6,11 +7,17 @@ const cartSlice = createSlice({
     itemsList: [],
     totalQuantity: 0,
     showCart: false,
+    changed: false,
   },
   reducers: {
+    replaceData(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.itemsList = action.payload.itemsList;
+    },
     addToCart(state, action) {
+      state.changed = true;
       const newItem = action.payload;
-      // to check if the item is already available
+      // to check if item is already available
       const existingItem = state.itemsList.find(
         (item) => item.id === newItem.id
       );
@@ -30,22 +37,18 @@ const cartSlice = createSlice({
       }
     },
     removeFromCart(state, action) {
+      state.changed = true;
       const id = action.payload;
-      // to check if the item is already available
-      const existingItem = state.itemsList.find(
-        (item) => item.id === id
-      );
+
+      const existingItem = state.itemsList.find((item) => item.id === id);
       if (existingItem.quantity === 1) {
-        state.itemsList = state.itemsList.filter(item => item.id !== id);
-       
+        state.itemsList = state.itemsList.filter((item) => item.id !== id);
+        state.totalQuantity--;
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice -= existingItem.price
+        existingItem.totalPrice -= existingItem.price;
       }
-
-
     },
-
     setShowCart(state) {
       state.showCart = !state.showCart;
     },
